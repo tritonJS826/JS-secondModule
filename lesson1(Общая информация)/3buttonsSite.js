dd3c55a80b255bc1f737b0a55927e9294a6ff6d6
triton <triton.68@mail.ru>
2019-10-26 10:08:25 +0300

    1homeworkComplete

--- lesson1(Общая информация)/3buttonsSite.js
+++ lesson1(Общая информация)/3buttonsSite.js
@@ -9,14 +9,26 @@ butDelAll.addEventListener("click", delAll);
 function changeStyle(){
   document.body.style.backgroundImage = "url('forest.jpg')";
   butChangeStyle.style.background = "black";
-  butChangeStyle.style.padding = "20px 20px";
-  butChangeStyle.style.color= "white"
+  butChangeStyle.style.display = "flex";
+  butChangeStyle.style.width = "15%";
+  butChangeStyle.style.height = "15%";
+  butChangeStyle.style.justifyContent = "center";
+  butChangeStyle.style.alignContent = "center";
+  butChangeStyle.style.color = "white";
   butToOtherPage.style.background = "black";
-  butToOtherPage.style.padding = "20px 20px";
-  butToOtherPage.style.color= "white"
+  butToOtherPage.style.display = "flex";
+  butToOtherPage.style.width = "15%";
+  butToOtherPage.style.height = "15%";
+  butToOtherPage.style.justifyContent = "center";
+  butToOtherPage.style.alignContent = "center";
+  butToOtherPage.style.color = "white";
   butDelAll.style.background ="black";
-  butDelAll.style.padding = "20px 20px";
-  butDelAll.style.color= "white";
+  butDelAll.style.display = "flex";
+  butDelAll.style.width = "15%";
+  butDelAll.style.height = "15%";
+  butDelAll.style.justifyContent = "center";
+  butDelAll.style.alignContent = "center";
+  butDelAll.style.color = "white";
 }
 
 function goToOtherPage(){
@@ -25,39 +37,48 @@ function goToOtherPage(){
 
 function delAll(){
   document.body.innerHTML = "";
-  document.body.style.background = "white";
-  document.body.innerHTML += '<div id="menu">menu</div>'+
+  document.body.innerHTML += '<HTML><body id="body">'+
+  '<div id="menu">menu</div>'+
   '<div id="mainBlock" >mainBlock</div>'+
-  '<footer id="footer">footer info</footer>';
+  '<footer id="footer">footer info</footer>'+
+  '</body></HTML>';
+  //let body = document.getElementById("body");
+  document.body.style.cssText='display : flex;' +
+  'flex-direction : column;' +
+  'width : 99%;' +
+  'justify-content : space-between;'+
+  'align-items: center;'+
+  'background-image : url("air.jpg");'+
+  'height:97%;';
+  //
+
   let menu = document.getElementById("menu");
-  menu.style.cssText='background-color : rgba(250,50,170,0.9);'+
-  'text-align : center;'+
-  'line-height : 10vh;'+
+  menu.style.cssText='background-color : rgba(250,250,170,0.5);'+
   'display : flex;'+
-  'flex-direction : row;'+
+  'flex-direction : column;'+
   'justify-content : space-around;'+
   'align-items : center;'+
-  'position : fixed;'+
   'border-style : solid;'+
-  'margin-left : 0;'+
-  'width : 98%;'+
-  'height : 20vh;'+
-  'margin-top : 0px;';
+  'width : 100%;'+
+  'min-height : 20%;' +
+  'align-self : flex-start;'+
+  'flex:0 0 15%;';
   let mainBlock = document.getElementById("mainBlock");
-  mainBlock.style.cssText='background-color : rgba(250,50,170,0.9);'+
-  'text-align : center;'+
-  'display : block;'+
-  'position : absolute;'+
-  'top : 25%;'+
-  'left : 25%;'+
-  'width : 50%;'+
-  'height : 50vh;';
+  mainBlock.style.cssText='background-color : rgba(250,200,170,0.5);'+
+  'display : flex;'+
+  'flex-direction : column;'+
+  'justify-content : space-around;'+
+  'align-items : center;'+
+  'width : 50%;' +
+  'flex:0 0 30%;';
   let footer = document.getElementById("footer");
-  footer.style.cssText='background-color : rgba(100,200,100,0.9);'+
+  footer.style.cssText='background-color : rgba(100,200,100,0.5);'+
   'border : 5px solid black;'+
-  'position : fixed;'+
-  'height : 10vh;'+
-  'bottom : 0vh;'+
-  'text-align : center;'+
-  'width : 98%;';
-}
+  'display : flex;'+
+  'flex-direction : column;'+
+  'justify-content : center;'+
+  'align-items : center;'+
+  'width : 100%;' +
+  'flex:0 0 15%;'+
+  'position:relative;'
+  }
